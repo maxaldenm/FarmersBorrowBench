b47f9c959ffd6b05eb2641d97e53d74041cae527
Max <Max@4500U>
2021-03-09 09:56:59 +0100

    initial commit

---
+++ src/main/java/com/miw/farmersborrowbench/FarmersborrowbenchApplication.java
@@ -0,0 +1,13 @@
+package com.miw.farmersborrowbench;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmersborrowbenchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FarmersborrowbenchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/miw/farmersborrowbench/FarmersborrowbenchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miw.farmersborrowbench;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmersborrowbenchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
