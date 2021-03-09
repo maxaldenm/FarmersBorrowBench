61cb66d72251075217cb6b9ea38525147d9878d4
BasF <b.fluitman@student.fontys.nl>
2021-03-09 15:21:55 +0100

    crap

--- src/main/java/com/miw/farmersborrowbench/controllers/DefaultModelAttributeController.java
+++ src/main/java/com/miw/farmersborrowbench/controllers/DefaultModelAttributeController.java
@@ -1,20 +1,14 @@
 package com.miw.farmersborrowbench.controllers;
 
-import com.mit.farmersborrowbank.beans.Login;
-import com.mit.farmersborrowbank.beans.User;
+import com.miw.farmersborrowbench.beans.User;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 @ControllerAdvice
 public class DefaultModelAttributeController {
 
-    @ModelAttribute("newuser")
+    @ModelAttribute("user")
     public User getDefaultUser(){
         return new User();
-    }
-
-    @ModelAttribute("login")
-    public Login getDefaultLogin(){
-        return new Login();
     }
 }
